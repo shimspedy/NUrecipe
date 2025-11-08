766aee3190937e79b70fa672f20b8bc1ccb7132f
john <36556216+shimspedy@users.noreply.github.com>
2025-11-08 12:06:14 -0500

    Configure server to use layout system
    - Import express-ejs-layouts middleware
    - Set 'layout.ejs' as the default layout
    - All views now automatically use the layout
    - This fixes Bootstrap CSS not loading issue

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 // Set EJS as template engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
+app.set('layout', 'layout');
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
